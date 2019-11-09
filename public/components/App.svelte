8668aebde8b23b34d69d21ba116f6b6ac8719f0c
DeMarc Johnson <demarcjohnson@gmail.com>
2019-11-08 23:58:44 -0500

    migrating the from rollup to parcel

---
+++ public/components/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Header from './header.svelte';
+</script>
+
+
+
+<Header/>
+  <main>
+  <h1>Portfolio</h1>
+  <p>When looking at these sites use the inspector or the view source to check the top of either layout.css file or the script.js to see my signutare</p>
+    <ul class="site_list">
+      <li class="sites"><a href="http://www.waxahachie.com/">Waxahachie, TX</a></li>
+      <li class="sites"><a href="http://avonpark.cc/">Avon Park, FL</a></li>
+      <li class="sites"><a href="https://www.villageofmatteson.org/">mattesonil</a></li>
+      <li class="sites"><a href="https://www.uppermarlboromd.gov/">Upper Marlboro, MD</a></li>
+      <li class="sites"><a href="https://www.co.pine.mn.us/">Pine County, MN</a></li>
+      <li class="sites"><a href="https://www.franklin.mi.us/">Franklin, MI</a></li>
+      <li class="sites"><a href="https://www.somersetmd.us/">Somerset County, MD</a></li>
+      <li class="sites"><a href="http://www.ameliacova.com/">Amelia County, VA</a></li>
+      <li class="sites"><a href="http://www.cartercountyok.us/">Carter County, OK</a></li>
+      <li class="sites"><a href="http://www.blueashadvance.com/">blueashedcoh</a></li>
+      <li class="sites"><a href="http://www.milfordtownship.com/">Milford Township, MI</a></li>
+      <li class="sites"><a href="https://www.cariboucountysheriff.com/">Caribou County Sheriff</a></li>
+      <li class="sites"><a href="https://www.cityoflakeport.com/">Lakeport, CA</a></li>
+      <li class="sites"><a href="https://www.allendalecounty.com/">Allendale County, SC</a></li>
+      <li class="sites"><a href="https://www.cityofelcampo.org/">El Campo, TX</a></li>
+      <li class="sites"><a href="https://www.bellavistaar.gov/">Bella Vista, AR</a></li>
+      <li class="sites"><a href="https://oglecounty.org">Ogle County, IL</a></li>
+    </ul>
+  </main>
